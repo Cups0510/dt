8f11b38aa08a76a6822b3688530fb6883b321d1b
Cups0510 <118185410+Cups0510@users.noreply.github.com>
2023-03-10 11:31:35 +0530

    Add files via upload

---
+++ employee/views.py
@@ -0,0 +1,76 @@
+from typing import List, Optional
+from ninja import Router, Query, Schema
+from django.shortcuts import get_object_or_404
+from pydantic.fields import Field
+from pydantic.types import conint
+import spacy
+
+
+from common.schema import Message
+from employee.models import Employee
+from employee.schemas import EmployeeIn, EmployeeOut
+
+router = Router(tags=['employees'])
+
+
+class Filters(Schema):
+    first_name__contains: str = Field(None, alias="fisrt_name")
+    last_name__contains: str = Field(None, alias="last_name")
+    department_id: Optional[conint(ge=0)]
+
+
+@router.post("/employees")
+def create_employee(request, payload: EmployeeIn):
+    employee = Employee.objects.create(**payload.dict())
+    return {"id": employee.id}
+
+
+@router.get("/employees/{employee_id}", response={200: EmployeeOut, 404: Message})
+def get_employee(request, employee_id: int):
+    """
+    - :param employee_id:
+    - :return:
+        - 200返回正常
+        - 404找不到对象
+    """
+    employee = get_object_or_404(Employee, id=employee_id)
+    return employee
+
+
+@router.get("/employees", response=List[EmployeeOut])
+def list_employees(request, filters: Filters = Query(...)):
+    qs = Employee.objects.filter(**filters.dict(exclude_none=True))
+    print(qs.query)
+    return qs
+
+
+@router.put("/employees/{employee_id}")
+def update_employee(request, employee_id: int, payload: EmployeeIn):
+    employee = get_object_or_404(Employee, id=employee_id)
+    for attr, value in payload.dict().items():
+        setattr(employee, attr, value)
+    employee.save()
+    return {"success": True}
+
+
+@router.delete("/employees/{employee_id}")
+def delete_employee(request, employee_id: int):
+    employee = get_object_or_404(Employee, id=employee_id)
+    employee.delete()
+    return {"success": True}
+
+@router.get("/dtdt/{question}/{employee_idxx}")
+def dtx(request, question: str, employee_idxx: str):
+
+    nlp = spacy.load("en_core_web_lg")
+    # s1 = nlp.vocab[w1]
+    # s2 = nlp.vocab[w2]
+
+    s1 = nlp(question)
+    s2 = nlp(employee_idxx)
+    s1.similarity(s2)
+    print(s1.similarity(s2))
+    s3 = s1.similarity(s2)
+
+    return s3
+
